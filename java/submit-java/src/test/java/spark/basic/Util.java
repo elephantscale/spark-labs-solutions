c0c6dad1b69731c56f1e7cc884fb26a043e930e2
Mark Kerzner <mark@elephantscale.com>
2019-05-24 14:51:15 -0500

    java

---
+++ java/submit-java/src/test/java/spark/basic/Util.java
@@ -0,0 +1,32 @@
+package spark.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Util {
+    public static List<String> runUnixCommand(String command) {
+        ArrayList<String> output = new ArrayList<String>();
+        String s= "";
+        try {
+
+            Process p = Runtime.getRuntime().exec(command);
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            // read the output from the command
+            while ((s = stdInput.readLine()) != null) {
+                output.add(s);
+            }
+            // read any errors from the attempted command
+            while ((s = stdError.readLine()) != null) {
+                output.add(s);
+            }
+        } catch (IOException e) {
+            System.out.println("Could not run the following command: " + command);
+            System.out.println(s);
+        }
+        return output;
+    }
+}
