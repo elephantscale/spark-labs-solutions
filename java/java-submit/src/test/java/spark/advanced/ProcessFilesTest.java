da70b92b696c3aa66bd02694f95d924d04d20532
Mark Kerzner <mark@elephantscale.com>
2019-05-24 14:22:07 -0500

    restructure labs

---
+++ java/java-submit/src/test/java/spark/advanced/ProcessFilesTest.java
@@ -0,0 +1,25 @@
+package spark.advanced;
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class ProcessFilesTest {
+    private String testRun = "/Users/markkerzner/spark/bin/spark-submit" +
+            " --class 'spark.advanced.ProcessFiles'" +
+            " --master local[*]" +
+            " --executor-memory 4g" +
+            "  --driver-class-path logging/" +
+            "  target/spark.advanced-2.11-jar-with-dependencies.jar" +
+            " /data/text/twinkle/1G.data";
+    @Test
+    public void testSomething() {
+        assertTrue(true);
+    }
+    @Test
+    public void testSparkSubmit() {
+        List<String> output = Util.runUnixCommand(testRun);
+        System.out.println(output);
+    }
+}
