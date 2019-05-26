c2ce9cbcdaad55bc056e0bcbb315a80cb3486d93
Amir Rajabi <infomanian@users.noreply.github.com>
2019-05-27 01:28:46 +0200

    Update ProcessFilesTest.java

--- java/submit-java/src/test/java/spark/basic/ProcessFilesTest.java
+++ java/submit-java/src/test/java/spark/basic/ProcessFilesTest.java
@@ -1,27 +1,30 @@
 package spark.basic;
 import org.junit.Test;
-
 import java.io.IOException;
-import java.util.List;
 
 
-import static junit.framework.TestCase.assertTrue;
+//import static junit.framework.TestCase.assertTrue;
 
 public class ProcessFilesTest {
     String home = System.getProperty("user.home");
     String project = System.getProperty("user.dir");
     private String testRun = home +
-            "/spark/bin/spark-submit" +
-            " --class 'spark.basic.ProcessFiles'" +
-            " --master local[*]" +
-            " --executor-memory 4g " +
-            project + "/" +  "target/spark.basic-2.11-jar-with-dependencies.jar" +
-            " /data/text/twinkle/1G.data";
+          "/spark/bin/" +
+           "spark-submit" + " && " +
+           "class 'spark.basic.ProcessFiles'" + " && " +
+           "master local[*]" + " && " +
+           "executor-memory 4g " +
+           project + "/" +  "target/spark.basic-2.11-jar-with-dependencies.jar" +
+           " /data/text/twinkle/1G.data";
+
     @Test
     public void testSparkSubmit() throws IOException {
         System.out.println("testSparkSubmit");
-        List<String> output = Util.runUnixCommand(testRun);
-        System.out.println(output.size());
-        System.out.println(output.toString());
+        //List<String> output = Util.runUnixCommand(testRun);
+
+        // if you don't run it on windows deleter 'cmd /c' from the following command
+        Runtime.getRuntime().exec("cmd /c start cmd.exe /k " + testRun);//, null, selectedFile.getParentFile());
+        //System.out.println(output);
+        //System.out.println(output.length());
     }
 }
