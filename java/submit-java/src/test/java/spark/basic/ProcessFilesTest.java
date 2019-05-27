89b3b4589964384ed7e49bdd557988f944c6effd
Mark Kerzner <mark@elephantscale.com>
2019-05-26 19:43:11 -0500

    SPARK LABS: first labs with test fixed for good

--- java/submit-java/src/test/java/spark/basic/ProcessFilesTest.java
+++ java/submit-java/src/test/java/spark/basic/ProcessFilesTest.java
@@ -1,6 +1,10 @@
 package spark.basic;
 import org.junit.Test;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
 
 
 //import static junit.framework.TestCase.assertTrue;
@@ -9,22 +13,17 @@ public class ProcessFilesTest {
     String home = System.getProperty("user.home");
     String project = System.getProperty("user.dir");
     private String testRun = home +
-          "/spark/bin/" +
-           "spark-submit" + " && " +
-           "class 'spark.basic.ProcessFiles'" + " && " +
-           "master local[*]" + " && " +
-           "executor-memory 4g " +
-           project + "/" +  "target/spark.basic-2.11-jar-with-dependencies.jar" +
+          "/spark/bin/spark-submit" +
+           " --class spark.basic.ProcessFiles" +
+           " " + project + "/" +  "target/spark.basic-2.11.jar" +
            " /data/text/twinkle/1G.data";
 
     @Test
     public void testSparkSubmit() throws IOException {
         System.out.println("testSparkSubmit");
-        //List<String> output = Util.runUnixCommand(testRun);
-
-        // if you don't run it on windows deleter 'cmd /c' from the following command
-        Runtime.getRuntime().exec("cmd /c start cmd.exe /k " + testRun);//, null, selectedFile.getParentFile());
-        //System.out.println(output);
-        //System.out.println(output.length());
+        List<String> output = Util.runUnixCommand(testRun);
+        String first = output.get(0);
+        System.out.println(first);
+        assertTrue(first.contains("count: 31,300,777"));
     }
 }
